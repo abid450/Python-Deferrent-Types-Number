d1bfc6dd08ff0e5ce0c9b846b237e75299db51cd
Abid Hasan <144882936+abid450@users.noreply.github.com>
2023-10-23 21:35:25 +0600

    I have a Upload Python file

---
+++ main.py
@@ -0,0 +1,46 @@
+
+# Deffrent Types of Number --------------------
+
+num1 = int (input("Enter number 1 : "))
+num2 = int (input("Enter number 2 :"))
+print("Minus Number -----------------")
+num3 = int (input("Enter number 3 :"))
+num4 = int (input("Enter number 4 :"))
+print("Into Number -----------------")
+num5 = int (input("Enter number 5 :"))
+num6 = int (input("Enter number 6 :"))
+print("Devition Number--------------")
+num7 = int (input("Enter number 7 :"))
+num8 = int (input("Enter number 8 :"))
+print("Devided Number --------------")
+num9 = int (input("Enter number 9 :"))
+num10 = int (input("Enter number 10 :"))
+
+
+print("----------------------------")
+
+result = num1 + num2
+print("The Sum Number : ",result)
+
+print("------------------------")
+
+result = num3 - num4
+print("The Minus  Number : ",result)
+
+print("--------------------------")
+
+result = num5 * num6
+print("The Into Number : ",result)
+
+print("------------------------")
+
+result = num7 / num8
+print("The Devition Number : ",result)
+
+print("--------------------------")
+
+result = num7 % num8
+print("The Devided Number : ",result)
+
+
+
